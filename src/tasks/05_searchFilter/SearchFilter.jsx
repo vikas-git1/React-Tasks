a83f4a6f1f182ffe7bf792d49aacf396410a28ff
vikas-git1 <vikasmewara.dev@gmail.com>
2025-08-17 17:47:30 +0530

    Search Validation Setup

---
+++ src/tasks/05_searchFilter/SearchFilter.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const items = [
+  "Apple",
+  "Banana",
+  "Orange",
+  "Mango",
+  "Grapes",
+  "Pineapple",
+  "Strawberry",
+  "Watermelon",
+  "Kiwi",
+  "Papaya",
+  "Blueberry",
+  "Cherry",
+  "Peach",
+  "Lemon",
+  "Guava",
+];
+
+const SearchFilter = () => {
+  return (
+    <div>
+      <input type="text" />
+    </div>
+  );
+};
+
+export default SearchFilter;
