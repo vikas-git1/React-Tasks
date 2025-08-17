388852f8c685c194763933716c3737069a6a26a6
vikas-git1 <vikasmewara.dev@gmail.com>
2025-08-17 16:47:00 +0530

    Form Validation

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "./App.css";
 import Counter from "./tasks/01_counter/Counter";
 import TodoApp from "./tasks/02_todo/TodoApp";
 import PhotoGallery from "./tasks/03_fetchData/PhotoGallery";
+import FormValidation from "./tasks/04_formValidation/FormValidation";
 
 function App() {
   return (
     <div className="min-h-[100vh] w-[100vw] p-20 bg-gray-600 border-4 border-amber-50 ">
       {/* <Counter /> */}
       {/* <TodoApp /> */}
-      <PhotoGallery />
+      {/* <PhotoGallery /> */}
+      <FormValidation />
     </div>
   );
 }
